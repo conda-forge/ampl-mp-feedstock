c9bdb7815470fe10b429477ecdf76cb20868203e
Silvio Traversaro <silvio@traversaro.it>
2020-12-20 12:42:39 +0100

    Create bld.bat

---
+++ recipe/bld.bat
@@ -0,0 +1,22 @@
+setlocal EnableDelayedExpansion
+
+mkdir build
+cd build
+
+cmake -G "NMake Makefiles" ^
+      -DBUILD_SHARED_LIBS=True ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ^
+      -DCMAKE_FIND_FRAMEWORK=LAST ^
+      -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=True ^
+      -DCMAKE_BUILD_TYPE:STRING=None ^
+      -Wno-dev ^
+      ..
+if errorlevel 1 exit 1
+
+nmake VERBOSE=1
+if errorlevel 1 exit 1
+
+nmake install
+if errorlevel 1 exit 1
